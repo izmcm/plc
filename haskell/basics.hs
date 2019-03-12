f80e4230372969d481a08e0c57070b98cc998775
izmcm <cmeloizabella@gmail.com>
2019-03-12 11:40:45 +0000

    insert basics programs

--- haskell/basics.hs
+++ haskell/basics.hs
@@ -1,6 +1,19 @@
-fat :: Int -> Int
+fat :: Integer -> Integer
 fat n | n == 0    = 1
       | otherwise = n*fat(n - 1)
+           
+sumList :: [Int] -> Int
+sumList []     = 0
+sumList (x:xs) = x + sumList (xs)
+    
+doubleList :: [Int] -> [Int]
+doubleList []     = []
+doubleList (x:xs) = (2*x):doubleList xs
 
-main = do
-    putStrLn (show (fat 4))
+memberList :: [Int] -> Int -> Bool
+memberList [] x = False
+memberList (a:as) x | x == a    = True
+                    | otherwise = memberList as x
+
+
+main = do print (memberList [300, 100, 1] 1)
