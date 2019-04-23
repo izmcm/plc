bcc81d5d62e55c4ac79fc8e7016b35f9cf47e4f0
izmcm <cmeloizabella@gmail.com>
2019-04-23 12:05:08 +0000

    organizando

--- haskell/aulas/sort.hs
+++ haskell/aulas/sort.hs
@@ -2,6 +2,6 @@
 
 quicksort :: [Int] -> [Int]
 quicksort [] = []
-quicksort (a:as) = quicksort [x | x <- as, x <= a] ++ [a] ++ quicksort [y | y <- as, y > a]
+quicksort (a:as) = quicksort [x | x <- as, x < a] ++ [a] ++ quicksort [y | y <- as, y > a]
 
 main = do print(quicksort [5,1,20,7,20,20])
